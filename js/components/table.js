3e1060afc1a9f6301243d9e7c03dcf89cc67cb3f
Juan Ferreira <encjuanfr2006@gmail.com>
2025-01-29 19:16:04 -0400

    correção de erros geral

--- js/components/table.js
+++ js/components/table.js
@@ -1,35 +1,39 @@
 import { userGastos } from '../storage.js';
 
-export const isLenghtZero = () => {
-    if (userGastos.length === 0) {
-        const table = document.querySelector('table');
-        table.classList.add('opacity-25', 'select-none')
-    } else {
-        const rowExample = document.querySelector('#row-example');
-        rowExample.classList.add('hidden')
-    }
-}
-
-
-// erro exibição dos gastos na tabela
-export const mostrarGastos = () => {
+export const atualizarTabela = () => {
+    const table = document.querySelector('table');
     const tableBody = document.querySelector("#table-body");
-    userGastos.map(item => 
-        tableBody.innerHTML += `
-            <tr id="row-example" class="bg-white border-b overflow-x-hidden dark:bg-zinc-900 dark:border-zinc-800 border-gray-200">
-                <th scope="row"
-                    class="w-10 px-6 py-4 font-medium text-gray-900 dark:text-white">
-                    ${item.nome}
-                </th>
-                <td class=" px-6 hidden md:block py-4 underline">
-                    <a href="#">${item.link}</a>
-                </td>
-                <td class="px-6 py-4">${item.data}</td>
-                <td class="px-6 py-4">R$${item.valor}</td>
-            </tr>
-        `
-    )
+    const tableFoot = document.querySelector("#table-foot")
+
+    if (userGastos.length > 0) {
+        table.classList.remove('opacity-25')
+    }
+
+    // erro exibição dos gastos na tabela
+    tableBody.innerHTML = "";
+
+    userGastos.map(item => {
+        tableBody.innerHTML += 
+        `<tr class="bg-white border-b overflow-x-hidden dark:bg-zinc-900 dark:border-zinc-800 border-gray-200">
+            <th scope="row" class="w-50% md:w-25% px-6 py-4 font-medium text-gray-900 dark:text-white">
+            ${item.nome}
+            </th>
+            <td class="px-6 hidden md:table-cell py-4 underline">
+                <a href="${item.link}" class="h-full" target="_blank">Abrir link</a>
+            </td>
+            <td class="px-6 place-self-start py-4 hidden sm:table-cell">
+                ${item.data}
+            </td>
+            <td class="px-6 py-4">
+                R$${item.valor}
+            </td>
+        </tr>`;
+    });
+
+    const valorTotal = userGastos
+        .map(item => item.valor)
+        .reduce((acumulador, valorInicial) => acumulador + valorInicial, 0)
+    tableFoot.innerHTML = `Total: R$${Math.floor(valorTotal, 2)}`
 }
 
-mostrarGastos();
-isLenghtZero();
+console.log('table.js: ok')
--- js/storage.js
+++ js/storage.js
@@ -1,12 +1,12 @@
 export const userGastos = [
-    {nome: "a", link: "hdasd", data: "01/02/2014", valor: 1000},
-    {nome: "b", link: "bbbbb", data: "01/02/2014", valor: 2000}
+    // {nome: "a", link: "hdasd", data: "01/02/2014", valor: 1000},
+    // {nome: "b", link: "bbbbb", data: "01/02/2014", valor: 2000}
 ];
 
 export const addGasto = (nome, link, data, valor) => {
     userGastos.push(
-        {nome: nome, link: link, data: data, valor: valor}
+        {nome: String(nome), link: link, data: data, valor: Number(valor)}
     )
 }
 
-console.log(userGastos)
+console.log('storage.js: ok')
